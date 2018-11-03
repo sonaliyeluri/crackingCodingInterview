25cf8630e9fe04cedad40ffd704998e9520fb34a
Sonali Yeluri <sonaliyeluri@yahoo.com>
2018-11-03 12:07:01 -0700

    Is string s2 rotation of string s1 - Second Commit

--- isStringRotate.c
+++ isStringRotate.c
@@ -4,8 +4,10 @@
 
 bool isRotate(char *s1, char *s2) {
 	int len = strlen(s1);
+	char s12[2*len];
 	if (len == strlen(s2) && len > 0) {
-		if (strcmp(strcat(s1,s1), s2) > 0)
+		strcpy(s12, s1);
+		if (strcmp(strcat(s12,s1), s2) > 0)
 			return 1;
 	}
 	return 0;
@@ -13,7 +15,7 @@ bool isRotate(char *s1, char *s2) {
 
 int main() {
 	char s1[] = "waterbottle";
-	char s2[] = "erbottlewat";
+	char s2[] = "erwat";
 	printf("%s",isRotate(s1, s2)?"true":"false");
 	printf("\n");
 	return 0;
